65dffe6ba5bbed66d433a5400876a1dadc4ab8d1
Khushi-Agrawal-1 <khushi.agrawal2019@vitstudent.ac.in>
2021-07-02 19:22:05 +0530

    day9 completed

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:application_1/pages/login_page.dart';
 import 'package:application_1/utils/routes.dart';
 import 'package:application_1/widgets/themes.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+
 
 void main(){
   runApp(MyApp());
---
+++ lib/models/catalog.dart
@@ -0,0 +1,18 @@
+class Item{
+  final String id;
+  final String name;
+  final String desc;
+  final int price;
+  final String color;
+  final String image;
+
+  Item(this.id, this.name, this.desc, this.price, this.color, this.image);
+
+}
+
+final products =[
+ Item("001", "iPhone 12 pro", "Apple iPhone 12th generation", 100000, "#33505a", "https://fdn.gsmarena.com/imgroot/reviews/20/apple-iphone-12-pro-max/lifestyle/-1200w5/gsmarena_008.jpg")
+ 
+
+
+];
